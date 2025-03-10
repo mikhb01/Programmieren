e2967930ef03341716714861e6ce7a34a49fb27c
mikhb01 <lengertleon@gmail.com>
2025-03-11 00:11:46 +0900

    Weitere Modiul durch

--- Mammothinteractive-beginner-python/testfile.py
+++ Mammothinteractive-beginner-python/testfile.py
@@ -1,51 +1,101 @@
-# myName="Leon"
-# print(myName)
+# # myName="Leon"
+# # print(myName)
 
-# numberOfPets = 3
-# print(numberOfPets)
+# # numberOfPets = 3
+# # print(numberOfPets)
 
-# ##Challenge
-# myAge = 23
-# print(myAge)
+# #########################
 
-# ##Challenge 2
-# challenge = -5.0
-# print(type(challenge))
+# # ##Challenge
+# # myAge = 23
+# # print(myAge)
 
-# boolean = 4==4
-# print(boolean)
+# # ##Challenge 2
+# # challenge = -5.0
+# # print(type(challenge))
 
-# booleanString = "dog" == "dog"
-# print(booleanString)
+# ############################
 
-# falseBoolean = "cat" == "pig"
-# print(falseBoolean)
+# # boolean = 4==4
+# # print(boolean)
 
-# notBoolean = "true"
-# print(not)
+# # booleanString = "dog" == "dog"
+# # print(booleanString)
 
-# test = True
-# print(test)
-# print(type(test))
+# # falseBoolean = "cat" == "pig"
+# # print(falseBoolean)
 
-##Challenge 3
-# checkBoolean = "pig" == "cat"
-# print(checkBoolean)
-# print(type(checkBoolean))
+# # notBoolean = "true"
+# # print(not)
 
-# pet = "corgi"
-# age = 2
-# print(f"Hello, {pet}. You are {age} years old.")
+# # test = True
+# # print(test)
+# # print(type(test))
 
-# weather = "sunny"
-# temperatute = 40
-# print(f"Today is {weather}. It is {temperatute} degrees")
+# ##Challenge 3
+# # checkBoolean = "pig" == "cat"
+# # print(checkBoolean)
+# # print(type(checkBoolean))
 
-# coins = 100
-# job = "seller"
-# print(f"I was a {job} at the lemonade stand. I made {coins} cents")
+# #####################################
 
-# #Challenge 4
-# favClass1 = "Physics"
-# favClass2 = "Math"
-# print(f"My favourite classes were {favClass1} and {favClass2}.")
+# # pet = "corgi"
+# # age = 2
+# # print(f"Hello, {pet}. You are {age} years old.")
+
+# # weather = "sunny"
+# # temperatute = 40
+# # print(f"Today is {weather}. It is {temperatute} degrees")
+
+# # coins = 100
+# # job = "seller"
+# # print(f"I was a {job} at the lemonade stand. I made {coins} cents")
+
+# # #Challenge 4
+# # favClass1 = "Physics"
+# # favClass2 = "Math"
+# # print(f"My favourite classes were {favClass1} and {favClass2}.")
+
+# #################
+
+# myAge = 13
+# mySisterAge = 15
+# myAge + mySisterAge
+
+
+# 100 + 345
+
+# cakePieces = 8
+# eatenPieces = 4
+# currentPieces = cakePieces - eatenPieces
+
+# eggs = 12
+# crackedEggs = 5
+# eggs - crackedEggs
+
+# quarterValue = 0.25
+# quarters = 12
+# quarterValue * quarters
+
+# secInMin = 60
+# minInH = 60
+# hInD = 24
+
+# secInMin * minInH * hInD
+
+# pSlices = 8
+# people = 3
+
+# pSlices / people
+
+# money = 300
+# people = 5
+# money / people
+
+
+# #Challenge 5
+# (5 * 7) / 2
+# 5 * 7 / 2
+
+# ################
+
