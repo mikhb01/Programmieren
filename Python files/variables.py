6338560c46de2de1ad078efc4874bc1de66a0c08
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-09-08 09:59:27 +0900

    new project

---
+++ Python files/variables.py
@@ -0,0 +1,7 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+first_name = "Leon"
+
+print(first_name)
+print(f"Hallo, {first_name}")
