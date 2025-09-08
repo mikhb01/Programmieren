63d3bac19248db8bda171a05895511706fd53590
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-09-08 11:27:38 +0900

    if, if, if...

---
+++ Python files/if_statements.py
@@ -0,0 +1,19 @@
+# if = Do some code only IF  some condition is TRUE
+#      ELSE do something  else
+
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You are now signed up")
+# elif age < 0:
+#     print("You haven't been born yet!")
+# else: 
+#     print("You must be 18+ to sign up")
+
+
+response = input("Do you want some food? (Y/N): ")
+
+if response == "Y":
+    print("Have some food!")
+else: 
+    print("No food for you!")
