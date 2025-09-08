ada2feb45ea241fe2f080d731343cf9678c6afaa
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-09-08 11:01:10 +0900

    mathe, wooooh

---
+++ Python files/arithmetics_and_maths.py
@@ -0,0 +1,42 @@
+import math
+
+friends = 5
+
+# friends = friends + 1
+# friends += 1
+
+# friends = friends - 2
+# friends -= 2
+
+# friends = friends * 3
+# friends *= 3
+
+# friends = friends / 2
+# friends /= 2
+
+# friends = friends ** 2
+# friends **= 2
+
+# remainder = friends % 3
+
+# print(remainder)
+
+
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(y, 3)
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+##Stuff related to the imported math function
+# math.pi
+# print(math.e)
+
+# result = math.sqrt(y)
+result = math.ceil(z)
+
+print(result)
--- Python files/exercises.py
+++ Python files/exercises.py
@@ -9,10 +9,45 @@
 
 #Exercise 2 Shopping cart program
 
-item = input("What item would you like to buy?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
-total = price * quantity
+# item = input("What item would you like to buy?: ")
+# price = float(input("What is the price?: "))
+# quantity = int(input("How many would you like?: "))
+# total = price * quantity
 
-print(f"You have bought {quantity} x {item}/s")
-print(f"Your total is: €{total}")
+# print(f"You have bought {quantity} x {item}/s")
+# print(f"Your total is: €{total}")
+
+# Exercise 3 Circumference of a circle
+# import math
+
+# # Formel = U(mfang) = 2 * pi * r(adius) [C(ircumference) = 2 * pi * r(adius)]
+
+# radius = float(input("Enter the radius of a circle: "))
+
+# circumference = 2 * math.pi * radius
+
+# print(f"The circumference is: {round(circumference, 2)}cm.")
+
+#Exercise 4 Area of a circle
+#Formel A(rea) = pi * r(adius)²
+
+# import math
+
+# radius = float(input("Enter the radius of a circle: "))
+
+# area = math.pi * pow(radius, 2)
+
+# print(f"The area of the circle is {round(area, 2)}cm²")
+
+
+# Exercise 5 Hypothenus of a triangle
+# Formel: a² + b² = c² / (sqrt of a² + b²) = c
+
+import math
+
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"Side c = {round(c, 2)}")
