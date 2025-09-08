4555ff0402f48a0cab0a9570daf7d36b70790051
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-09-08 10:30:47 +0900

    user_input, typecasting and a little exercise added

---
+++ Python files/typecasting.py
@@ -0,0 +1,23 @@
+# Typecasting = the process of converting a variable from one data type to another
+#                   str(), int(), float(), bool()
+
+name = "Leon Lengert"
+age = 23
+gpa = 3.0
+is_student = True
+
+# gpa = int(gpa)
+
+# print(gpa)
+
+# age = float(age)
+
+# print(age)
+
+# age = str(age)
+
+# print(age)
+
+# name = bool(name)
+
+# print(name)
