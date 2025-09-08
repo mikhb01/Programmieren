57a250f8d1c771fad34cd6399c7ba03eae97f1a1
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-09-08 10:10:46 +0900

    variables done

--- Python files/variables.py
+++ Python files/variables.py
@@ -1,7 +1,33 @@
 # Variable = A container for a value (string, integer, float, boolean)
 #            A variable behaves as if it was the value it contains
 
-first_name = "Leon"
 
-print(first_name)
-print(f"Hallo, {first_name}")
+#Strings
+first_name = "Leon"
+food = "Lasagne"
+
+# print(f"Hallo, {first_name}")
+# print(f"Du magst {food}")
+
+
+#Integers
+age = 25
+
+# print(f"Du bist {age} Jahre alt")
+
+
+#Float
+
+price = 10.99
+
+# print(f"The price is €{price}")
+
+# Boolean (first letter needs to be capital)
+is_student = False
+
+# print(f"Are you a student?: {is_student}")
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
