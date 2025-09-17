c46f8eb3e472477e8f7847a0b5424cc6ba2cd11d
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-09-17 13:19:17 +0900

    new stuff

---
+++ Python files/for_loop.py
@@ -0,0 +1,14 @@
+# for-loop = execute a block of code a fixed number of times.
+#            you can iterate over a range, string, sequence, etc.
+
+# credit_card = "1234-5678-9012-3456"
+
+# for x in credit_card:
+#     print(x)
+
+for x in range(1, 21):
+    if x == 13:
+        # continue
+        break
+    else:
+        print(x)
