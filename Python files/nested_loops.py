e7ffad112cd44e56cbfa95c585644f2780d57b0f
mikhb01 <lengertleon@gmail.com>
2025-10-07 18:18:47 +0900

    yeahh

--- Python files/nested_loops.py
+++ Python files/nested_loops.py
@@ -2,5 +2,16 @@
 #                 outer loop:
 #                     inner loop:
 
-for x in range(1, 10):
-    print(x)
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter a symbol to use: ")
+
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end="")
+    print()
+
+
+
+    
