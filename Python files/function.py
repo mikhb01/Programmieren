f69b8e4988f6b52ca98b0bc325e48a8a683964da
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-10-25 02:17:15 +0200

    function & return intro

---
+++ Python files/function.py
@@ -0,0 +1,20 @@
+# function = A block of reusable code
+#           place () after the function name to invoke it
+
+
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}!")
+    print(f"You are {age}")
+    print("Happy birthday to you!")
+    print()
+
+
+# happy_birthday("Bro", 20)
+
+
+def display_invoice(usernamee, amount, due_date):
+    print(f"Hello {usernamee}")
+    print(f"Your bill of €{amount} is due: {due_date}")
+
+# display_invoice("Leon", 42.50, "01/01")
+
