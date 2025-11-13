05e4660e1d5ca2351985544f7ab45c1a153adaa5
mikhb01 <lengertleon@gmail.com>
2025-11-13 17:01:17 +0100

    neue daten

--- Python files/Data_science_from _scratch/Chapter_1_Key_connector.py
+++ Python files/Data_science_from _scratch/Chapter_1_Key_connector.py
@@ -1,7 +1,12 @@
+# Data Science from Scratch, (O'Reilly)
 # Content of chapter of p. 4 - 
 
 from __future__ import division
-from collections import Counter
+from collections import Counter, defaultdict
+
+################################
+# Finding key connectors
+################################
 
 users = [
         { "id": 0, "name": "Hero"},
@@ -56,6 +61,62 @@ num_friends = sorted(num_friends_by_id,             # sorting
 
 # each pair is (user_id, num_friends)
 
+################################
+# Data Scientists you may know
+################################
+
+interests = [
+          (0, "Hadoop"), 
+          (0, "Big Data"), 
+          (0, "HBase"), 
+          (0, "Java"),
+          (0, "Spark"), 
+          (0, "Storm"), 
+          (0, "Cassandra"),
+          (1, "NoSQL"), 
+          (1, "MongoDB"), 
+          (1, "Cassandra"), 
+          (1, "HBase"),
+          (1, "Postgres"), 
+          (2, "Python"), 
+          (2, "scikit-learn"), 
+          (2, "scipy"),
+          (2, "numpy"), 
+          (2, "statsmodels"), 
+          (2, "pandas"), 
+          (3, "R"), 
+          (3, "Python"),
+          (3, "statistics"), 
+          (3, "regression"), 
+          (3, "probability"), 
+          (4, "machine learning"), 
+          (4, "regression"), 
+          (4, "decision trees"),
+          (4, "libsvm"), 
+          (5, "Python"), 
+          (5, "R"), 
+          (5, "Java"), 
+          (5, "C++"),
+          (5, "Haskell"), 
+          (5, "programming languages"), 
+          (6, "statistics"),
+          (6, "probability"), 
+          (6, "mathematics"), 
+          (6, "theory"),
+          (7, "machine learning"), 
+          (7, "scikit-learn"), 
+          (7, "Mahout"),
+          (7, "neural networks"), 
+          (8, "neural networks"), 
+          (8, "deep learning"),
+          (8, "Big Data"), 
+          (8, "artificial intelligence"), 
+          (9, "Hadoop"),
+          (9, "Java"), 
+          (9, "MapReduce"), 
+          (9, "Big Data")
+]
+
 def friends_of_friends_id_bad(user):
      # foaf is short for 'friend of a friend'
      return[foaf["id"]
@@ -86,3 +147,88 @@ def friends_of_friends_ids(user):
                     and not_friends(user, foaf))    # and are't my friends
 
 print(friends_of_friends_ids(users[3]))             # Counter({0: 2, 5: 1})
+
+def data_scientists_who_like(target_interest):
+     return(user_id 
+            for user_id, user_interest in interests
+            if user_interest == target_interest)
+
+# print(data_scientists_who_like('Java'))
+
+# keys are interests, values are lists of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+     user_ids_by_interest[interest].append(user_id)
+
+# print(user_ids_by_interest['Java'])
+
+# userts to interest
+
+# keys are ids, values are lists of interests for user_id
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+     interests_by_user_id[user_id].append(interest)
+
+print(interests_by_user_id[0])
+
+def most_common_interests_with(user):
+     return Counter(interests_by_user_id 
+                    for interest in interests_by_user_id[user["id"]]
+                    for interests_by_user_id in user_ids_by_interest[interest]
+                    if interests_by_user_id != user["id"])
+
+# print(most_common_interests_with(users[0]))
+
+################################
+# Sales and Experience
+################################
+
+salaries_and_tenures = [
+     (83000, 8.7), 
+     (88000, 8.1),
+     (48000, 0.7), 
+     (76000, 6),
+     (69000, 6.5), 
+     (76000, 7.5),
+     (60000, 2.5), 
+     (83000, 10),
+     (48000, 1.9), 
+     (63000, 4.2)
+     ]
+
+# keys are in years, values are lists of the salaries for each tenure
+salary_by_tenure = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+     salary_by_tenure[tenure].append(salary)
+
+# keys are years, each value is average salary for that tenure
+average_salary_by_tenure = {
+     tenure : sum(salaries) /  len(salaries)
+     for tenure, salaries in salary_by_tenure.items()
+}
+
+def tenure_bucket(tenure):
+     if tenure < 2:
+          return "less than two"
+     elif tenure < 5:
+          return "between two and five"
+     else:
+          return "more than five"
+     
+# keys are tenure bucket, values are lists of salaries for that bucket
+salary_by_tenure_bucket = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+     bucket = tenure_bucket(tenure)
+     salary_by_tenure_bucket[bucket].append(salary)
+
+# keys are tenure buckets, values is average salaries for that bucket
+average_salary_by_bucket = {
+     tenure_bucket : sum(salaries) / len(salaries)
+     for tenure, salaries in salary_by_tenure_bucket.items()
+}
+
+print(average_salary_by_bucket)
