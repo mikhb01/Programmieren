3028383e2e86ad31b442f9444fd8f0b9a7929612
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2026-03-28 18:37:52 +0100

    paar seiten mehr gemacht

--- PythonBuch/Kapitel1/datentypen.py
+++ PythonBuch/Kapitel1/datentypen.py
@@ -1,3 +1,22 @@
 print(42 + 12.0 * False)
 
 # Weiter bei Seite 46
+
+zeichenkette = "-" * 10
+print(zeichenkette)
+
+eine_zahl = int("42") + 23
+print(eine_zahl)
+
+ein_text = "42" + str(23)
+print(ein_text)
+
+eggs = 20
+spam = "Jeder Tag hat " + str(4 + eggs) + " Stunden"
+print(spam)
+
+eggs2 = int("2" + "4")
+spam2 = "Jeder Tag hat " + str(eggs2 + 12) + " Stunden"
+print(spam2)
+
+# weiter mit seite 51
