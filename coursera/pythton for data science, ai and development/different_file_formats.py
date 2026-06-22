776ebc299d94cbc1cb8fd8c4d067b57dbe96a317
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2026-06-23 01:35:52 +0200

    new stuff

---
+++ coursera/pythton for data science, ai and development/different_file_formats.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pyodide.http import pyfetch
+
+filename = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/addresses.csv"
+
+async def download(url, filename):
+    response = await pyfetch(url)
+    if response.status == 200:
+        with open(filename, "wb") as f:
+            f.write(await response.bytes())
+
+download(filename, "addresses.csv")
+
+
+df = pd.read_csv("addresses.csv", header=None)
